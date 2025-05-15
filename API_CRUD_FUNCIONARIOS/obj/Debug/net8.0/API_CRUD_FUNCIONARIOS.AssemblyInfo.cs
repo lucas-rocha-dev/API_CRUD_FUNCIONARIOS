e50972a01bb6d5d34f9399e35ec215d1f776a51b
Lucas Rodrigues <lucas_luri@hotmail.com>
2025-05-14 23:10:17 -0300

    feat: img endpoints

--- API_CRUD_FUNCIONARIOS/obj/Debug/net8.0/API_CRUD_FUNCIONARIOS.AssemblyInfo.cs
+++ API_CRUD_FUNCIONARIOS/obj/Debug/net8.0/API_CRUD_FUNCIONARIOS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_CRUD_FUNCIONARIOS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6966e9d4c05dfa16c55d8fa05808b2d70354ecdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1db8d9141a2e5d4d1dbaf5097d714a991d0b5654")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_CRUD_FUNCIONARIOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_CRUD_FUNCIONARIOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
